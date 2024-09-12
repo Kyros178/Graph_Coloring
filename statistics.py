ecb5a2fa6f25f3f08200cdf23655de9652252864
Kyros178 <nils@beckmann-bi.de>
2024-09-12 20:12:44 +0200

    Outputs erweitert so das ich statistiken zur berechnung unter verschiedenen Knoten zahlen, regularitäten und farben anzahlen sammeln kann + png dieser daten

--- statistics.py
+++ statistics.py
@@ -92,7 +92,9 @@ if __name__ == "__main__":
 
     #print(len( list(graphs[8][4][0].neighbors(1)) ))
 
-    
+
+    print("Knoten")
+    # Knotenzahl verändern !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     negCount=0
     posCount=0
     runtimes_brut = []
@@ -100,8 +102,12 @@ if __name__ == "__main__":
     runtimes_opt = []
 
     #nur bis n = 9 da sonst zu viele lange tests
-    parameter_values =range(5,10)
+    #todo wieder auf 9 bringen durch range(5,10) 
+    parameter_values =range(5,9)
 
+    numGraphs = [len(graphs[n][4]) for n in parameter_values]
+
+    
     for n in parameter_values:
         #todo richtig implementieren und überprüfen
         #todo zeiten durch anzahl graphen teilen
@@ -110,6 +116,7 @@ if __name__ == "__main__":
         runtimes_brut.append(bruteForce_time)
         runtimes_bad.append(CSPSolverBad_time)
         runtimes_opt.append(CSPSolverOpt_time)
+        
         for g in graphs[n][4]:
             for cAM in colMat[3][4]:
                 adjGraph = nx.to_numpy_array(g)
@@ -129,6 +136,12 @@ if __name__ == "__main__":
     bar_width = 0.25  # Breite der Balken
     index = np.arange(len(parameter_values))  # x-Positionen für die Gruppen
 
+    runtimesPerGraph_brut = [int(a / b) for a, b in zip(runtimes_brut, numGraphs)]  
+    runtimesPerGraph_bad  = [int(a / b) for a, b in zip(runtimes_bad, numGraphs)]  
+    runtimesPerGraph_opt =  [int(a / b) for a, b in zip(runtimes_opt, numGraphs)]   
+
+
+    #plot 1 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     # Barplots erstellen
     plt.figure(figsize=(12, 6))
     plt.bar(index, runtimes_brut, bar_width, label='Brute Force', color='skyblue')
@@ -138,10 +151,185 @@ if __name__ == "__main__":
     # Achsen und Titel beschriften
     plt.xlabel('Parameter (n)')
     plt.ylabel('Laufzeit (Sekunden)')
-    plt.title('Laufzeitvergleich von drei Funktionen')
+    plt.title('Laufzeitvergleich der Solver für alle Graphen mit n Knoten zusammen')
+    plt.xticks(index + bar_width, [str(f"Anzahl Knoten: {n}\n Anzahl Graphen: {total}") for n, total in zip(parameter_values, numGraphs) ])
+    plt.legend()
+    
+    # Plot anzeigen
+    plt.tight_layout()
+    plt.savefig('z_anzKnoten_Gesamt_plot.png')
+    plt.show()
+
+    plt.close()
+    #plot 2 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    
+    # Barplots erstellen
+    plt.figure(figsize=(12, 6))
+    plt.bar(index, runtimesPerGraph_brut, bar_width, label='Brute Force', color='skyblue')
+    plt.bar(index + bar_width, runtimesPerGraph_bad, bar_width, label='CSP mit schlechten Constraints', color='lightgreen')
+    plt.bar(index + 2 * bar_width, runtimesPerGraph_opt, bar_width, label='CSP optimiert', color='salmon')
+    
+    # Achsen und Titel beschriften
+    plt.xlabel('Parameter (n)')
+    plt.ylabel('Laufzeit (Sekunden)')
+    plt.title('Laufzeitvergleich der Solver durchschnitt pro Graph mit n Knoten  ')
     plt.xticks(index + bar_width, [str(n) for n in parameter_values])
     plt.legend()
     
     # Plot anzeigen
     plt.tight_layout()
+    plt.savefig('z_anzKnoten_Einzeln_plot.png')
     plt.show()
+
+    
+
+    plt.close()
+    
+
+    print("regularität")
+    #Regularität verändern !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    runtimes_brut = []
+    runtimes_bad = []
+    runtimes_opt = []
+
+    #nur bis n = 9 da sonst zu viele lange tests
+    #todo wieder auf 9 bringen durch range(5,10) 
+    parameter_values =range(3,6)
+
+    numGraphs = [len(graphs[8][r]) for r in parameter_values]
+
+    
+    for r in parameter_values:
+        #todo richtig implementieren und überprüfen
+        #todo zeiten durch anzahl graphen teilen
+        # graph mit n Knoten reg= 4 und 3 färbungen
+        (bruteForce_time,CSPSolverBad_time,CSPSolverOpt_time) =vergleicher(graphs[8][r], colMat[3][r])
+        runtimes_brut.append(bruteForce_time)
+        runtimes_bad.append(CSPSolverBad_time)
+        runtimes_opt.append(CSPSolverOpt_time)
+        
+    bar_width = 0.25  # Breite der Balken
+    index = np.arange(len(parameter_values))  # x-Positionen für die Gruppen
+
+    runtimesPerGraph_brut = [int(a / b) for a, b in zip(runtimes_brut, numGraphs)]  
+    runtimesPerGraph_bad  = [int(a / b) for a, b in zip(runtimes_bad, numGraphs)]  
+    runtimesPerGraph_opt =  [int(a / b) for a, b in zip(runtimes_opt, numGraphs)]   
+
+
+    #plot 1 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # Barplots erstellen
+    plt.figure(figsize=(12, 6))
+    plt.bar(index, runtimes_brut, bar_width, label='Brute Force', color='skyblue')
+    plt.bar(index + bar_width, runtimes_bad, bar_width, label='CSP mit schlechten Constraints', color='lightgreen')
+    plt.bar(index + 2 * bar_width, runtimes_opt, bar_width, label='CSP optimiert', color='salmon')
+    
+    # Achsen und Titel beschriften
+    plt.xlabel('Parameter (r)')
+    plt.ylabel('Laufzeit (Sekunden)')
+    plt.title('Laufzeitvergleich der Solver für alle Graphen mit regularität r zusammen')
+    plt.xticks(index + bar_width, [str(f"regularität: {r}\n Anzahl Graphen: {total}") for r, total in zip(parameter_values, numGraphs) ])
+    plt.legend()
+    
+    # Plot anzeigen
+    plt.tight_layout()
+    plt.savefig('z_reg_Gesamt_plot.png')
+    plt.show()
+
+    plt.close()
+    #plot 2 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    
+    # Barplots erstellen
+    plt.figure(figsize=(12, 6))
+    plt.bar(index, runtimesPerGraph_brut, bar_width, label='Brute Force', color='skyblue')
+    plt.bar(index + bar_width, runtimesPerGraph_bad, bar_width, label='CSP mit schlechten Constraints', color='lightgreen')
+    plt.bar(index + 2 * bar_width, runtimesPerGraph_opt, bar_width, label='CSP optimiert', color='salmon')
+    
+    # Achsen und Titel beschriften
+    plt.xlabel('Parameter (r)')
+    plt.ylabel('Laufzeit (Sekunden)')
+    plt.title('Laufzeitvergleich der Solver durchschnitt pro Graph mit regularität r  ')
+    plt.xticks(index + bar_width, [str(r) for r in parameter_values])
+    plt.legend()
+    
+    # Plot anzeigen
+    plt.tight_layout()
+    plt.savefig('z_reg_Einzeln_plot.png')
+    plt.show()
+
+
+
+
+    
+    plt.close()
+
+    print("Färbungen")
+    #Farbzahl verändern verändern !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    runtimes_brut = []
+    runtimes_bad = []
+    runtimes_opt = []
+
+    #nur bis n = 9 da sonst zu viele lange tests
+    #todo wieder auf 9 bringen durch range(5,10) 
+    parameter_values =range(2,5)
+
+    numGraphs = [len(graphs[8][4]) for f in parameter_values]
+
+    
+    for f in parameter_values:
+        #todo richtig implementieren und überprüfen
+        #todo zeiten durch anzahl graphen teilen
+        # graph mit n Knoten reg= 4 und 3 färbungen
+        (bruteForce_time,CSPSolverBad_time,CSPSolverOpt_time) =vergleicher(graphs[8][4], colMat[f][4])
+        runtimes_brut.append(bruteForce_time)
+        runtimes_bad.append(CSPSolverBad_time)
+        runtimes_opt.append(CSPSolverOpt_time)
+        
+    bar_width = 0.25  # Breite der Balken
+    index = np.arange(len(parameter_values))  # x-Positionen für die Gruppen
+
+    runtimesPerGraph_brut = [int(a / b) for a, b in zip(runtimes_brut, numGraphs)]  
+    runtimesPerGraph_bad  = [int(a / b) for a, b in zip(runtimes_bad, numGraphs)]  
+    runtimesPerGraph_opt =  [int(a / b) for a, b in zip(runtimes_opt, numGraphs)]   
+
+
+    #plot 1 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # Barplots erstellen
+    plt.figure(figsize=(12, 6))
+    plt.bar(index, runtimes_brut, bar_width, label='Brute Force', color='skyblue')
+    plt.bar(index + bar_width, runtimes_bad, bar_width, label='CSP mit schlechten Constraints', color='lightgreen')
+    plt.bar(index + 2 * bar_width, runtimes_opt, bar_width, label='CSP optimiert', color='salmon')
+    
+    # Achsen und Titel beschriften
+    plt.xlabel('Parameter (f)')
+    plt.ylabel('Laufzeit (Sekunden)')
+    plt.title('Laufzeitvergleich der Solver für alle Graphen mit f Farben')
+    plt.xticks(index + bar_width, [str(f"anz. Farben: {f}\n Anzahl Graphen: {total}") for f, total in zip(parameter_values, numGraphs) ])
+    plt.legend()
+    
+    # Plot anzeigen
+    plt.tight_layout()
+    plt.savefig('z_anzFarben_Gesamt_plot.png')
+    plt.show()
+
+    plt.close()
+    #plot 2 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    
+    # Barplots erstellen
+    plt.figure(figsize=(12, 6))
+    plt.bar(index, runtimesPerGraph_brut, bar_width, label='Brute Force', color='skyblue')
+    plt.bar(index + bar_width, runtimesPerGraph_bad, bar_width, label='CSP mit schlechten Constraints', color='lightgreen')
+    plt.bar(index + 2 * bar_width, runtimesPerGraph_opt, bar_width, label='CSP optimiert', color='salmon')
+    
+    # Achsen und Titel beschriften
+    plt.xlabel('Parameter (f)')
+    plt.ylabel('Laufzeit (Sekunden)')
+    plt.title('Laufzeitvergleich der Solver durchschnitt pro Graph mit f Farben ')
+    plt.xticks(index + bar_width, [str(f) for f in parameter_values])
+    plt.legend()
+    
+    # Plot anzeigen
+    plt.tight_layout()
+    plt.savefig('z_anzFarben_Einzeln_plot.png')
+    plt.show()
+
+
+
